c3b71c1e7d97d130b8ccae9b83d71596b991e83b
Luciano <lucianovilasboas@gmail.com>
2024-11-10 15:20:33 -0300

    Atualização de texto

--- Home.py
+++ Home.py
@@ -6,10 +6,27 @@ def main():
     st.title("Fórum França-Brasil")
     st.header("Sobre o Fórum")
     st.write("""
-    O Fórum França-Brasil é um evento anual que reúne especialistas, acadêmicos, 
+    O Fórum França-Brasil é um evento bi-anual que reúne especialistas, acadêmicos, 
     empresários e autoridades dos dois países para discutir temas de interesse comum. 
     O objetivo é fortalecer as relações bilaterais e promover a cooperação em diversas áreas, 
     como economia, cultura, ciência e tecnologia.
+             
+    Após seu encontro em junho de 2023 em Paris, os Presidentes Emmanuel Macron e Luiz Inácio Lula da Silva decidiram organizar uma Temporada Brasil-França em 2025, para dar novo impulso à relação bilateral, que celebrará 200 anos em 2025, e fortalecer as nossas ações comuns face aos desafios políticos, sociais e ecológicos contemporâneos. Neste espírito, a Temporada favorecerá projetos baseados em parcerias entre instituições e organizações francesas e brasileiras, suscetíveis de se prolongarem para além de 2025 e de fortalecerem os laços entre as nossas sociedades. Multidisciplinar, inovadora e comprometida, a Temporada contará com uma primeira parte brasileira na França, de abril a setembro, precedida de um prelúdio a partir de janeiro, e uma segunda parte francesa no Brasil, de agosto a dezembro de 2025.
+
+    As prioridades definidas pelos dois presidentes para esta Temporada são as seguintes: clima e transição ecológica; diversidade das sociedades e diálogo com África; democracia e globalização equitativa.
+
+    O edital promovido pelo Institut Français trata da seleção e certificação de projetos no âmbito da programação francesa no Brasil. Está acessível no seguinte link, até sexta-feira 28 de junho de 2024 (23h59 – horário de Paris):
+
+    https://www.institutfrancais.com/fr/offre/france-bresil-2025
+
+    Este edital, em francês e inglês, está aberto a proponentes, franceses ou brasileiros, que queiram propor projetos que se inscrevam nesta programação, que será realizada de agosto a dezembro de 2025 no Brasil. Os projetos apresentados deverão envolver profissionais dos dois países, comprometidos em uma parceria.
+
+    Para qualquer dúvida, entre em contato com o Pôle Saisons et Temps forts do Institut Français: france-bresil@institutfrancais.com
+
+    A curadoria brasileira selecionará, em edital dedicado, os projetos no âmbito da programação brasileira na França. Para projetos realizados na França, os proponentes estão convidados a entrar em contato com a curadoria brasileira:
+    brasilfranca2025@itamaraty.gov.br
+
+    Texto extraído do site da [Embaixada da França no Brasil](https://br.ambafrance.org/Temporada-Franca-Brasil-2025).             
     """)
 
 if __name__ == "__main__":
